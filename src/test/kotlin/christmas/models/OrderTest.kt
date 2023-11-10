51563346c9449a98ef0df9d1d79d0e97f6f92ec1
minuxx <minux8905@gmail.com>
2023-11-11 00:45:42 +0900

    test: 주문 메뉴 타입 예외 처리 테스트 추가

--- src/test/kotlin/christmas/models/OrderTest.kt
+++ src/test/kotlin/christmas/models/OrderTest.kt
@@ -9,17 +9,33 @@ import org.junit.jupiter.api.assertThrows
 class OrderTest {
     @Test
     fun `주문한 메뉴의 총 개수가 20개 초과이면 예외가 발생한다`() {
-        val menu = listOf(Menu(MenuItem.BBQ_RIBS, 10), Menu(MenuItem.ZERO_COLA, 11))
+        val menus = listOf(Menu(MenuItem.BBQ_RIBS, 10), Menu(MenuItem.ZERO_COLA, 11))
 
-        val exception = assertThrows<IllegalArgumentException> { Order(menu) }
+        val exception = assertThrows<IllegalArgumentException> { Order(menus) }
 
         assertThat(exception.message).isEqualTo(ErrorMessage.INVALID_ORDER)
     }
 
     @Test
     fun `주문한 메뉴의 총 개수가 20개 이하면 예외가 발생하지 않는다`() {
-        val menu = listOf(Menu(MenuItem.BBQ_RIBS, 10), Menu(MenuItem.ZERO_COLA, 10))
+        val menus = listOf(Menu(MenuItem.BBQ_RIBS, 10), Menu(MenuItem.ZERO_COLA, 10))
 
-        assertDoesNotThrow { Order(menu) }
+        assertDoesNotThrow { Order(menus) }
+    }
+
+    @Test
+    fun `주문한 메뉴 항목들이 모두 음료인 경우이면 예외가 발생한다`() {
+        val menus = listOf(Menu(MenuItem.ZERO_COLA, 2), Menu(MenuItem.RED_WINE, 1))
+
+        val exception = assertThrows<IllegalArgumentException> { Order(menus) }
+
+        assertThat(exception.message).isEqualTo(ErrorMessage.INVALID_ORDER)
+    }
+
+    @Test
+    fun `주문한 메뉴 항목들이 모두 음료인 경우가 아니라면 예외가 발생한다`() {
+        val menus = listOf(Menu(MenuItem.BBQ_RIBS, 2), Menu(MenuItem.RED_WINE, 1))
+
+        assertDoesNotThrow { Order(menus) }
     }
 }
