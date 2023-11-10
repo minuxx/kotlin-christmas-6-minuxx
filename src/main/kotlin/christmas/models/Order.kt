502f1230b13d53b5fa6c578a6b8d120f8b81f9c5
minuxx <minux8905@gmail.com>
2023-11-11 00:35:30 +0900

    feat: 총주문 개수 예외 처리 기능 추가

--- src/main/kotlin/christmas/models/Order.kt
+++ src/main/kotlin/christmas/models/Order.kt
@@ -1,10 +1,16 @@
 package christmas.models
 
+import christmas.constants.ErrorMessage.INVALID_ORDER
+
 class Order(private val menu: List<OrderMenu>) {
 
     init {
-
+        require(validateTotalOrderCount()) { INVALID_ORDER }
     }
 
-    private fun validateOrderMenuCount() = 0
+    private fun validateTotalOrderCount() = menu.sumOf { it.count } <= MAX_TOTAL_ORDER_COUNT
+
+    companion object {
+        const val MAX_TOTAL_ORDER_COUNT = 20
+    }
 }
