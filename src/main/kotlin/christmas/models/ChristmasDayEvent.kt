7b33f8b322439140f4298560a3e3a54633b50a46
minuxx <minux8905@gmail.com>
2023-11-14 08:26:54 +0900

    test: 크리스마스 디데이 할인 금액 계산 테스트 추가

--- src/main/kotlin/christmas/models/ChristmasDayEvent.kt
+++ src/main/kotlin/christmas/models/ChristmasDayEvent.kt
@@ -2,25 +2,23 @@ package christmas.models
 
 import christmas.constants.ErrorMessage.INVALID_EVENT_DATE
 
-class ChristmasDayEvent(private val visitDate: Int) {
+class ChristmasDayEvent(private val eventDate: Int) {
     private var benefitAmount = PRINCIPAL
 
     init {
         require(validateEventDate()) { INVALID_EVENT_DATE }
+        benefitAmount = 0
     }
 
     fun benefitAmount() = benefitAmount
 
-    private fun calculateBenefitAmount() {
-
-    }
-
-    private fun validateEventDate() = visitDate in START_DATE..END_DATE
+    private fun validateEventDate() = eventDate in START_DATE..END_DATE
 
     companion object {
         const val START_DATE = 1
         const val END_DATE = 25
 
         private const val PRINCIPAL = 1000
+        private const val DISCOUNT_AMOUNT = 100
     }
 }
--- src/test/kotlin/christmas/models/ChristmasDayEventTest.kt
+++ src/test/kotlin/christmas/models/ChristmasDayEventTest.kt
@@ -8,18 +8,28 @@ import org.junit.jupiter.api.assertThrows
 
 class ChristmasDayEventTest {
     @Test
-    fun `방문날짜가 크리스마스 디데이 할인 기간이 아니면 예외가 발생한다`() {
-        val visitDate = 26
+    fun `이벤트날짜가 크리스마스 디데이 할인 기간이 아니면 예외가 발생한다`() {
+        val eventDate = 26
 
-        val exception = assertThrows<IllegalArgumentException> { ChristmasDayEvent(visitDate) }
+        val exception = assertThrows<IllegalArgumentException> { ChristmasDayEvent(eventDate) }
 
         assertThat(exception.message).isEqualTo(ErrorMessage.INVALID_EVENT_DATE)
     }
 
     @Test
-    fun `방문날짜가 크리스마스 디데이 할인 기간이면 예외가 발생하지 않는다`() {
-        val visitDate = 25
+    fun `이벤트날짜가 크리스마스 디데이 할인 기간이면 예외가 발생하지 않는다`() {
+        val eventDate = 25
 
-        assertDoesNotThrow { ChristmasDayEvent(visitDate) }
+        assertDoesNotThrow { ChristmasDayEvent(eventDate) }
+    }
+
+    @Test
+    fun `이벤트날짜에 해당하는 할인 금액을 계산한다`() {
+        val christmasDayEvent = ChristmasDayEvent(1)
+        val expectedBenefitAmount = 1000
+
+        val actualBenefitAmount = christmasDayEvent.benefitAmount()
+
+        assertThat(actualBenefitAmount).isEqualTo(expectedBenefitAmount)
     }
 }
