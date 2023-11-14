1cce59e2f5f150dc84b9b0c264dbd416e6ce3a34
minuxx <minux8905@gmail.com>
2023-11-14 14:17:48 +0900

    feat: 이벤트 리스트 생성 기능 추가

--- src/main/kotlin/christmas/models/EventGenerator.kt
+++ src/main/kotlin/christmas/models/EventGenerator.kt
@@ -4,6 +4,12 @@ class EventGenerator(private val date: Int, private val order: Order) {
     fun execute(): List<Event> {
         val events = mutableListOf<Event>()
 
+        create { ChristmasDDayEvent(date) }?.let { events.add(it)  }
+        create { WeekdayEvent(date, order.dessertMenuCount()) }?.let { events.add(it)  }
+        create { WeekendEvent(date, order.mainMenuCount()) }?.let { events.add(it)  }
+        create { SpecialEvent(date) }?.let { events.add(it)  }
+        create { PresentationEvent(date, order.amount()) }?.let { events.add(it)  }
+
         return events.toList()
     }
 
