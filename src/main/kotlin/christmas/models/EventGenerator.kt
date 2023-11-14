8ead5906ace2de519ddc617d85a452fa42801273
minuxx <minux8905@gmail.com>
2023-11-14 14:31:25 +0900

    test: 총주문 금액에 따른 이벤트 생성 처리 테스트 추가

--- src/main/kotlin/christmas/models/EventGenerator.kt
+++ src/main/kotlin/christmas/models/EventGenerator.kt
@@ -1,14 +1,20 @@
 package christmas.models
 
 class EventGenerator(private val date: Int, private val order: Order) {
+
     fun execute(): List<Event> {
         val events = mutableListOf<Event>()
+        val orderAmount = order.amount()
+
+        if (MINIMUM_ORDER_AMOUNT > orderAmount) {
+            return emptyList()
+        }
 
         create { ChristmasDDayEvent(date) }?.let { events.add(it)  }
         create { WeekdayEvent(date, order.dessertMenuCount()) }?.let { events.add(it)  }
         create { WeekendEvent(date, order.mainMenuCount()) }?.let { events.add(it)  }
         create { SpecialEvent(date) }?.let { events.add(it)  }
-        create { PresentationEvent(date, order.amount()) }?.let { events.add(it)  }
+        create { PresentationEvent(date, orderAmount) }?.let { events.add(it)  }
 
         return events.toList()
     }
@@ -19,5 +25,9 @@ class EventGenerator(private val date: Int, private val order: Order) {
         } catch (e: IllegalArgumentException) {
             null
         }
+    }
+
+    companion object {
+        private const val MINIMUM_ORDER_AMOUNT = 10_000
     }
 }
--- src/test/kotlin/christmas/models/EventGeneratorTest.kt
+++ src/test/kotlin/christmas/models/EventGeneratorTest.kt
@@ -1,13 +1,41 @@
 package christmas.models
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.assertj.core.api.AssertionsForClassTypes.assertThat
 
 class EventGeneratorTest {
+
     @Test
-    fun `크리스마스 디데이 이벤트 기간에 방문하면 이벤트가 생긴다`() {
+    fun `총 주문 금액이 10,000원 미만이면 이벤트를 생성하지 않는다`() {
+        val date = 1
+        val order = Order(listOf(
+            Menu(MenuItem.TAPAS, 1)
+        ))
+
+        val eventGenerator = EventGenerator(date, order)
+
+        val actualEvents = eventGenerator.execute()
+
+        assertTrue(actualEvents.isEmpty())
+    }
+
+    @Test
+    fun `총 주문 금액이 10,000원 이상이면 이벤트를 생성한다`() {
+        val date = 1
+        val order = Order(listOf(
+            Menu(MenuItem.TAPAS, 2)
+        ))
+
+        val eventGenerator = EventGenerator(date, order)
+
+        val actualEvents = eventGenerator.execute()
+
+        assertFalse(actualEvents.isEmpty())
+    }
+
+    @Test
+    fun `크리스마스 디데이 이벤트 기간에 방문하면 해당 이벤트를 생성한다`() {
         val date = 1
         val order = Order(listOf(
             Menu(MenuItem.BBQ_RIBS, 1)
@@ -17,5 +45,18 @@ class EventGeneratorTest {
         val actualEvents = eventGenerator.execute()
 
         assertTrue(actualEvents.any { it is ChristmasDDayEvent })
+    }
+
+    @Test
+    fun `크리스마스 디데이 이벤트 기간에 방문하지 않으면 해당 이벤트를 생성하지 않는다`() {
+        val date = 26
+        val order = Order(listOf(
+            Menu(MenuItem.BBQ_RIBS, 1)
+        ))
+        val eventGenerator = EventGenerator(date, order)
+
+        val actualEvents = eventGenerator.execute()
+
+        assertFalse(actualEvents.any { it is ChristmasDDayEvent })
     }
 }
