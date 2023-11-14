c5554e715bfd6c6d0ebdb8ee660254ead677afa4
minuxx <minux8905@gmail.com>
2023-11-14 13:03:06 +0900

    refactor: 이벤트 추상 클래스 적용해 PresentationEvent 개선

--- src/main/kotlin/christmas/models/PresentationEvent.kt
+++ src/main/kotlin/christmas/models/PresentationEvent.kt
@@ -1,28 +1,22 @@
 package christmas.models
 
-import christmas.constants.ErrorMessage.INVALID_EVENT_DATE
 import christmas.constants.ErrorMessage.INVALID_MINIMUM_ORDER_AMOUNT_FOR_PRESENTATION_EVENT
 
-class PresentationEvent(private val date: Int, private val orderAmount: Int) {
+class PresentationEvent(date: Int, private val orderAmount: Int) : Event(date) {
     private val menu: Menu = Menu(MenuItem.CHAMPAGNE, 1)
+    override var benefitAmount: Int = menu.amount()
 
     init {
-        require(isValidEventDate()) { INVALID_EVENT_DATE }
         require(isValidOrderAmount()) { INVALID_MINIMUM_ORDER_AMOUNT_FOR_PRESENTATION_EVENT }
     }
 
     fun menu() = menu.toString()
 
-    fun benefitAmount() = menu.amount()
-
-    private fun isValidEventDate() = date in START_DATE..END_DATE
+    override fun benefitAmount() = benefitAmount
 
     private fun isValidOrderAmount() = MINIMUM_ORDER_AMOUNT <= orderAmount
 
     companion object {
-        const val START_DATE = 1
-        const val END_DATE = 31
-
         private const val MINIMUM_ORDER_AMOUNT = 120_000
     }
 }
