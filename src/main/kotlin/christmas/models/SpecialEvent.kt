bdd4c54dc746afed428466b92cd0fbf9d527dbb8
minuxx <minux8905@gmail.com>
2023-11-14 13:05:25 +0900

    refactor: 이벤트 추상 클래스 적용해 SpecialEvent 개선

--- src/main/kotlin/christmas/models/SpecialEvent.kt
+++ src/main/kotlin/christmas/models/SpecialEvent.kt
@@ -2,22 +2,19 @@ package christmas.models
 
 import christmas.constants.ErrorMessage.INVALID_EVENT_DATE
 
-class SpecialEvent(private val date: Int) {
-    private var calculatedDiscount = DISCOUNT_AMOUNT
+class SpecialEvent(private val date: Int) : Event(date) {
+    override var benefitAmount: Int = DISCOUNT_AMOUNT
 
     init {
-        require(isValidSpecialDate()) { INVALID_EVENT_DATE }
+        require(isNotSpecialDate()) { INVALID_EVENT_DATE }
     }
 
-    fun discountAmount() = calculatedDiscount
+    override fun benefitAmount() = benefitAmount
 
-    private fun isValidSpecialDate() = (date in START_DATE..END_DATE) && date in specialDates
+    private fun isNotSpecialDate() = date !in specialDates
 
     companion object {
-        const val START_DATE = 1
-        const val END_DATE = 31
         private val specialDates = setOf(3, 10, 17, 24, 25, 31)
-
         private const val DISCOUNT_AMOUNT = 1000
     }
 }
