2bfab1e1caae8fac8b043b7dcc6d28231f1d978c
minuxx <minux8905@gmail.com>
2023-11-14 15:38:09 +0900

    feat: 할인 후 예상 결제 금액 출력 기능 추가

--- src/main/kotlin/christmas/views/OutputView.kt
+++ src/main/kotlin/christmas/views/OutputView.kt
@@ -35,6 +35,11 @@ class OutputView {
         println("-${CURRENCY_UNIT.format(totalBenefitAmount.withCommas())}" + NEW_LINE)
     }
 
+    fun printDiscountedAmount(discountedAmount: Int) {
+        println(TOTAL_BENEFIT_AMOUNT_MESSAGE)
+        println(CURRENCY_UNIT.format(discountedAmount.withCommas()) + NEW_LINE)
+    }
+
     companion object {
         private const val WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
         private const val PREVIEW_MESSAGE = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
@@ -45,5 +50,6 @@ class OutputView {
         private const val PRESENTATION_MENU_MESSAGE = "<증정 메뉴>"
         private const val BENEFIT_HISTORY_MESSAGE = "<혜택 내역>"
         private const val TOTAL_BENEFIT_AMOUNT_MESSAGE = "<총혜택 금액>"
+        private const val DISCOUNTED_AMOUNT_MESSAGE = "<할인 후 예상 결제 금액>"
     }
 }
