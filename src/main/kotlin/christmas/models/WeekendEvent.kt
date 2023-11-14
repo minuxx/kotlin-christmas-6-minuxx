e63cc6b6e40d436bffa8c233967eb4ca7328c993
minuxx <minux8905@gmail.com>
2023-11-14 10:45:42 +0900

    feat: 주말 할인 이벤트 날짜 예외 처리 기능 추가

--- src/main/kotlin/christmas/models/WeekendEvent.kt
+++ src/main/kotlin/christmas/models/WeekendEvent.kt
@@ -19,7 +19,12 @@ class WeekendEvent(private val eventDate: Int, private val mainMenuCount: Int = 
     private fun isValidEventDate() = eventDate in START_DATE..END_DATE
 
     private fun isWeekend(): Boolean {
-        return false
+        return try {
+            val eventDateTime = LocalDate.of(LocalDate.now().year, EVENT_MONTH, eventDate)
+            eventDateTime.dayOfWeek == DayOfWeek.SATURDAY || eventDateTime.dayOfWeek == DayOfWeek.SUNDAY
+        } catch (e: DateTimeException) {
+            false
+        }
     }
 
     companion object {
