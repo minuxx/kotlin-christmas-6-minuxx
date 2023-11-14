a121d500ed6876d61ef374aae040fe7160542269
minuxx <minux8905@gmail.com>
2023-11-14 10:32:14 +0900

    docs: 평일 할인 이벤트 날짜 예외 처리 기능 구현 완료

--- src/test/kotlin/christmas/models/WeekdayEventTest.kt
+++ src/test/kotlin/christmas/models/WeekdayEventTest.kt
@@ -2,7 +2,6 @@ package christmas.models
 
 import christmas.constants.ErrorMessage.INVALID_EVENT_DATE
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -11,7 +10,7 @@ class WeekdayEventTest {
 
     @ParameterizedTest
     @ValueSource(ints = [0, 16, 24]) // 범위, 토요일, 일요일
-    fun `평일 이벤트 날짜가 유효하지 않으면 예외가 발생한다`(eventDate: Int) {
+    fun `평일 할인 이벤트 날짜가 유효하지 않으면 예외가 발생한다`(eventDate: Int) {
         val exception = assertThrows<IllegalArgumentException> { WeekdayEvent(eventDate) }
 
         assertThat(exception.message).isEqualTo(INVALID_EVENT_DATE)
